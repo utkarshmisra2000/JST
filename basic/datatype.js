f0eb2d63f6c599ab8860b786bbaaa3099d2ebeb3
utkarshmisra2000 <80566466+utkarshmisra2000@users.noreply.github.com>
2024-03-04 02:59:08 +0000

    conversion

--- basic/datatype.js
+++ basic/datatype.js
@@ -3,3 +3,19 @@
 // alert (hii); 
 console.log(typeof null);
 console.log(typeof undefined);
+
+let score = "10";
+
+console.log(typeof score);
+
+let conversion = Number(score);
+
+console.log(typeof conversion);
+console.log(conversion);
+
+
+let ok = 20;
+
+let conv = Boolean(ok);
+
+console.log(conv);
