bf904a9e15fa22a613336fe3b5b1d779d2ada787
utkarshmisra2000 <80566466+utkarshmisra2000@users.noreply.github.com>
2024-03-04 02:46:47 +0000

    var and datatypes

---
+++ basic/datatype.js
@@ -0,0 +1,5 @@
+"use strict";
+
+// alert (hii); 
+console.log(typeof null);
+console.log(typeof undefined);
---
+++ basic/variables.js
@@ -0,0 +1,6 @@
+const accountId = 121212;
+let accountEmail = "abc@gmail.com";
+var accountPassword = "12345";
+accountCity = "jaipur";
+console.log(accountCity);
+console.table([accountId, accountEmail,accountPassword, accountCity]);
