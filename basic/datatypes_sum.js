b6223039796075373f2e93bf91dcd9c08c8fc265
utkarshmisra2000 <80566466+utkarshmisra2000@users.noreply.github.com>
2024-03-05 02:16:52 +0000

    stack and heap

--- basic/datatypes_sum.js
+++ basic/datatypes_sum.js
@@ -11,3 +11,34 @@
 const bigNum = 8451245124152451n;
 
 console.log(typeof bigNum);
+
+
+
+
+
+
+
+
+// MEMORIES........
+
+// two types : Stack(Primitive) and Heap(Reference).
+
+let myName = "Utkarsh"
+let yourName = myName
+
+yourName = "Ayush";
+
+console.log(myName, yourName);
+
+
+let app1 = {
+    id: 10,
+    name: "utkarsh"
+}
+
+let app2 = app1;
+
+app2.name = "Ayush";
+
+console.log(app1);
+console.log(app2);
