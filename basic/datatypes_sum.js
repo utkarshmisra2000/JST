9d685065843f3a050758fa425c24b88afab1ec8f
utkarshmisra2000 <80566466+utkarshmisra2000@users.noreply.github.com>
2024-03-05 02:04:58 +0000

    comp and datatype

---
+++ basic/datatypes_sum.js
@@ -0,0 +1,13 @@
+// Primitive datatypes(call by value)
+
+// 7 Types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// javascript is dynamically typed language.
+
+// Reference Type
+
+// Array, Objects, Functions
+
+const bigNum = 8451245124152451n;
+
+console.log(typeof bigNum);
---
+++ basic/operations.js
@@ -0,0 +1,4 @@
+let str = "hello";
+let str1 = " how are you";
+
+console.log(str + str1);
