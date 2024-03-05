13ad0db5224270159f024a449d3e0b70e6f2548e
utkarshmisra2000 <80566466+utkarshmisra2000@users.noreply.github.com>
2024-03-05 02:44:58 +0000

    string

---
+++ basic/String.js
@@ -0,0 +1,8 @@
+let firstName = "Utkarsh";
+let surName = "Mishra";
+let fullName = firstName+ " " + surName;
+
+console.log(fullName);
+
+
+console.log(`My name is ${firstName.toUpperCase()} ${surName.toUpperCase()}`);
