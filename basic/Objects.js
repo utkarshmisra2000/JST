342dab8c45ca35298f248501dc919d2c07fedf09
utkarshmisra2000 <80566466+utkarshmisra2000@users.noreply.github.com>
2024-04-01 00:23:19 +0000

    object

---
+++ basic/Objects.js
@@ -0,0 +1,9 @@
+const course ={
+    Name: "Javascript",
+    price: "1000",
+    course_teacher: "Hitesh"
+}
+const  {course_teacher: teach} = course;
+// console.log(course_teacher);
+console.log(teach);
+
